8900742006fcd1eba23df7e7cfbc3a609116f593
unknown <amandabeayoung@gmail.com>
2019-10-08 13:10:24 -0800

    Should have kept this on the main branch

--- ADC_script_test.R
+++
@@ -1,9 +0,0 @@
-###New script that I am writing for the arctic data center
-
-bg_chem<-read.csv(file = "BGchem2008data.csv" , stringsAsFactors = FALSE)
-
-head(bg_chem)
-
-library(ggplot2)
-
-ggplot(bg_chem, aes(CTD_Depth, CTD_Temperature))+geom_point()
